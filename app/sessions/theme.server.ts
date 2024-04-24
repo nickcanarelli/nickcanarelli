ab66c67bb680e484bebd9d2901da00f8479a7a85
Nick Canarelli <nick@nickcanarelli.com>
2024-04-23 21:51:58 -0400

    adding accurate production domain to cookie

--- app/sessions/theme.server.ts
+++ app/sessions/theme.server.ts
@@ -1,7 +1,6 @@
 import { createCookieSessionStorage } from '@remix-run/node'
 import { createThemeSessionResolver } from 'remix-themes'
 
-// You can default to 'development' if process.env.NODE_ENV is not set
 const isProduction = process.env.NODE_ENV === 'production'
 
 const sessionStorage = createCookieSessionStorage({
@@ -11,8 +10,10 @@ const sessionStorage = createCookieSessionStorage({
     httpOnly: true,
     sameSite: 'lax',
     secrets: ['s3cr3t'],
-    // Set domain and secure only if in production
-    ...(isProduction ? { domain: 'your-production-domain.com', secure: true } : {}),
+    /*
+     * Set domain and secure only if in production
+     */
+    ...(isProduction ? { domain: 'nickcanarelli.com', secure: true } : {}),
   },
 })
 
